5e56ba239dacbba4bfc796e90d7c666e8307d62e
Nandini21-cs <nandinishukla023@gmail.com>
2025-09-21 21:42:06 +0530

    Create firebase.ts

---
+++ frontend/src/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Replace with your Firebase config (from Firebase Console)
+const firebaseConfig = {
+  apiKey: "AIzaSyCW3MqG4nZA7ZYk4BXKxzFjnYfnKlqyU30",
+  authDomain: "ai-resume-analyzer-e01e1.firebaseapp.com",
+  projectId: "ai-resume-analyzer-e01e1",
+  storageBucket: "ai-resume-analyzer-e01e1.appspot.com",
+  messagingSenderId: "850290121597",
+  appId: "1:850290121597:web:e2aed469c96088b77d4a14",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
